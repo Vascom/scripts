83d92f5b2b4ff3ca32aeac2dbae1de0c0b8147a6
Vascom <vascom2@gmail.com>
2012-03-01 10:31:54 +0400

    Added smtp and variables

--- mail.sh
+++ mail.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+#/etc/mail.rc or ~/.mailrc
+#SMTP server for Ecotelecom provider
+#set smtp=smtp.ecotelecom.ru
+
 START_DATE=`date`
-echo "Компьютер solar запущен $START_DATE" | mail -s "solar started $START_DATE" vascom2@gmail.com
+SENSORS=`sensors`
+LOCAL_HOSTNAME=`hostname -s`
+
+echo -e "Компьютер $LOCAL_HOSTNAME запущен $START_DATE\n\n $SENSORS" | mail -s "$LOCAL_HOSTNAME started $START_DATE" vascom2@gmail.com
