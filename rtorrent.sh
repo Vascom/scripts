b37a03b1293985a432ed92b2d19c26afcd62e660
Vascom <vascom2@gmail.com>
2012-03-30 10:43:50 +0400

    Updates and cleanups

--- rtorrent.sh
+++ rtorrent.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-if [ -e /home/vascom/rt_sessions/rtorrent.lock ]
-then rm -f /home/vascom/rt_sessions/rtorrent.lock
+#HTTP bind
+if [ ! -e /var/www/lighttpd/downloads/list ]
+then
+    sudo mount --bind /home/vascom/Загрузки /var/www/lighttpd/downloads/
+    sudo mount -o remount,ro /var/www/lighttpd/downloads/
 fi
 
+#FTP start
+
+
+#WEBcam debug
+USBFS_MOUNTED=`mount | grep -c usbfs`
+if [ $USBFS_MOUNTED -ne "1" ]
+then
+    sudo mount -t usbfs none /proc/bus/usbfs
+fi
+
+#rtorrent start
+if [ -e /home/vascom/rt_sessions/rtorrent.lock ]
+then
+    rm -f /home/vascom/rt_sessions/rtorrent.lock
+fi
 screen -mdS rtorrent rtorrent
