a941a59931cac3068c702823e84712afd205df9e
Vascom <v.glazov@javad.com>
2013-06-11 09:47:41 +0400

    Change variables to lowcase naming

--- mail.sh
+++ mail.sh
@@ -4,21 +4,21 @@
 #SMTP server for Ecotelecom provider
 #set smtp=smtp.ecotelecom.ru
 
-START_STOP=1
+start_stop=1
 
-START_DATE=`date`
-SENSORS=`sensors`
-APC=`apcaccess`
-LOCAL_HOSTNAME=`hostname -s`
+start_date=`date`
+sensors=`sensors`
+apc=`apcaccess`
+local_hostname=`hostname -s`
 
 STATE_RU=остановлен
 STATE_EN=stop
 
-if [ $START_STOP == 1 ]
+if [ $start_stop == 1 ]
 then
     STATE_RU=запущен
     STATE_EN=start
 fi
 
-echo -e "Компьютер $LOCAL_HOSTNAME $STATE_RU $START_DATE\n\n $SENSORS\n\n $APC" |
-    mail -s "$LOCAL_HOSTNAME $STATE_EN $START_DATE" vascom2@gmail.com
+echo -e "Компьютер $local_hostname $STATE_RU $start_date\n\n $sensors\n\n $apc" |
+    mail -s "$local_hostname $STATE_EN $start_date" vascom2@gmail.com
