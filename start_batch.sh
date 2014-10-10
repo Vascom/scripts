bf37e6a73a57378f4fa968d25bc6ec7512743496
Vascom <vascom2@gmail.com>
2014-10-10 14:24:51 +0400

    Correct Channels number, colorizing output

--- start_batch.sh
+++ start_batch.sh
@@ -30,7 +30,7 @@ do
 done
 
 function send_email() {
-    ssh vascom@172.17.0.196 "DISPLAY=:0 notify-send Задание\ выполнено\ $1 Проект\ `basename $PWD`"
+    ssh vascom@172.17.0.236 "DISPLAY=:0 notify-send Задание\ выполнено\ $1 Проект\ `basename $PWD`"
 }
 
 function timer() {
@@ -52,6 +52,7 @@ CHAN_NUMBER_IN_WINDOW=`grep -m 1 CHAN_NUMBER_IN_WINDOW $ASIC_CONFIG | awk '{prin
 CHANNELS_NUMBER=`expr $WINDOWS_NUMBER \* $CHAN_NUMBER_IN_WINDOW`
 CHANNELS_NUMBER_EXT=`grep -m 1 CHANNELS_NUMBER_EXT $ASIC_CONFIG | awk '{print $4}' | cut -d ";" -f1`
 CHANNELS_QL_NUMBER=`grep -m 1 CHANNELS_QL_NUMBER $ASIC_CONFIG | awk '{print $4}' | cut -d ";" -f1`
+CHANNELS_NUMBER_ALL=`expr $CHANNELS_NUMBER + $CHANNELS_NUMBER_EXT`
 CHANNELS_QL_ENABLE=`grep -m 1 CHANNELS_QL_ENABLE $ASIC_CONFIG | awk '{print $4}' | cut -d ";" -f1 | cut -d "b" -f2`
 CHANNELS_QL=`expr $CHANNELS_QL_NUMBER \* $CHANNELS_QL_ENABLE`;
 FAST_ACQUISITION_LENGTH=`grep -m 1 FAST_ACQUISITION_LENGTH $ASIC_CONFIG | awk '{print $4}' | cut -d ";" -f1`
@@ -76,14 +77,14 @@ cd -
 echo -e "$GIT_COMMIT\n" | tee config_last
 
 echo -e "Date     : `date`
-    \rChannels : `expr $CHANNELS_NUMBER + $CHANNELS_NUMBER_EXT` + ${CHANNELS_QL}QL
+    \rChannels : $CHANNELS_NUMBER_ALL + ${CHANNELS_QL}QL
     \rFA Length: $FAST_ACQUISITION_LENGTH
     \rFilters  : $FILTERS_WB_NUMBER
     \rFFT ena  : $FFT_OUT_ENABLE" | tee --append config_last
 
 cat $ASIC_CONFIG >> config_last
 
-echo "Start mapping "
+echo -e "\e[32mStart\e[0m mapping "
 
 quartus_map $EN64 --parallel=on $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.map.log &
 timer map
@@ -91,10 +92,10 @@ timer map
 ERROR_PRESENT=`grep -c "Error:" "$PROJECT_NAME.map.rpt"`
 if [ $ERROR_PRESENT == 0 ]
 then
-    echo -e "\nStart merging"
+    echo -e "\n\e[32mStart\e[0m merging"
     quartus_cdb $EN64 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME --merge=on 2>&1 1>>longlog.map.log
 else
-    echo -e "\nFinished with errors in mapping"
+    echo -e "\n\e[31mFinished with errors in mapping\e[0m"
     if [[ $ERROR_PRESENT == 1 ]]
     then
         grep "Error " "$PROJECT_NAME.map.rpt"
@@ -106,11 +107,11 @@ fi
 ERROR_PRESENT=`grep -c "Error:" "$PROJECT_NAME.merge.rpt"`
 if [ $ERROR_PRESENT == 0 ]
 then
-    echo "Start fitting "
+    echo -e "\e[32mStart\e[0m fitting "
     quartus_fit $EN64 --parallel=4 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.fit.log &
     timer fit
 else
-    echo "Finished with errors in merging"
+    echo -e "\e[31mFinished with errors in merging\e[0m"
     send_email "с ошибкой в merging"
     exit
 fi
@@ -118,9 +119,9 @@ fi
 ERROR_PRESENT=`grep -c "Error:" "$PROJECT_NAME.fit.rpt"`
 if [ $ERROR_PRESENT == 0 ]
 then
-    echo -e "\nStart STA and assembling"
+    echo -e "\n\e[32mStart\e[0m STA and assembling"
     quartus_sta $EN64 --parallel=4 $PROJECT_NAME -c $PROJECT_NAME 1>/dev/null
-    quartus_asm $EN64 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME 1>/dev/null &
+    quartus_asm $EN64 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME 1>/dev/null
     head -n 2 $PROJECT_NAME.sta.rpt | tee --append config_last
     #head -n 159 $PROJECT_NAME.sta.rpt | tail -n 9
     grep "Restricted Fmax" "$PROJECT_NAME.sta.rpt" -m 1 -A 9 | cut -d ";" -f2,3,4 -s | tee --append config_last
@@ -129,15 +130,15 @@ then
     jic_seq_pre=`cat jic_sec_number`
     JIC_SEQ=`expr $jic_seq_pre + 1`
     echo $JIC_SEQ > jic_sec_number
-    JIC_NAME=`echo avt2_${JIC_SEQ}_chan${CHANNELS_NUMBER}_filt${FILTERS_WB_NUMBER}_fft${FFT_OUT_ENABLE}_mf${FAST_ACQUISITION_LENGTH}_${GIT_COMMIT_SHORT}`
+    JIC_NAME=`echo avt2_${JIC_SEQ}_chan${CHANNELS_NUMBER_ALL}_filt${FILTERS_WB_NUMBER}_fft${FFT_OUT_ENABLE}_mf${FAST_ACQUISITION_LENGTH}_${GIT_COMMIT_SHORT}`
     sed -i -e "s/<output_filename>.*<\/output_filename>/<output_filename>$JIC_NAME.jic<\/output_filename>/" $JIC_CONF
     quartus_cpf -c $JIC_CONF > /dev/null
     cp config_last $JIC_NAME.config
-    echo $JIC_NAME.jic
+    echo -e "\e[31m$JIC_NAME.jic\e[0m"
 
-    echo "All finished"
+    echo -e "\e[32mAll finished\e[0m"
     send_email "успешно"
 else
-    echo -e "\nFinished with errors in fitting"
+    echo -e "\n\e[31mFinished with errors in fitting\e[0m"
     send_email "с ошибкой в fitting"
 fi
