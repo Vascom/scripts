7c85d5ebf4d19520a34a0d0a041d58f89b3ffbcf
Vascom <vascom2@gmail.com>
2016-10-31 10:01:15 +0300

    Rename vars in fft_avt.

--- fft_avt.m
+++ fft_avt.m
@@ -28,8 +28,7 @@ end
 
 %Check if we in Octave or Matlab and load additional library for Octave
 vers = version;
-if (str2num(vers(1)) >= 7)
-else
+if (str2num(vers(1)) < 7)
     pkg load signal
 end
 
@@ -41,13 +40,14 @@ FS_FAST = 150;
 
 %Filename of input data
 %data_file = 'f0';
-%data must be in one or two (for complex mode) columns with " " delimiter
+%data must be in one or two (for complex mode) columns with " " (witespace) delimiter.
+%In Test mode enter 0.
 
 %Sampling frequency, MHz
 %Fs = 190;
 
 %Number of FFT points as power of 2
-%Low number - low Frequency resolution but big smoothnes of picture. Useful 9 to 14.
+%Low number - low Frequency resolution but more smoothnes of picture. Useful 9 to 14.
 %smpl = 14;
 
 %Enable Hann window for FFT
@@ -61,30 +61,32 @@ FS_FAST = 150;
 %================================================
 
 if test_mode == 1
-    f=1:2^20;
-    ssin = sin(f);
-    snoi = normrnd(0,0.1,2^20,1);
-    f4 = ssin' + snoi;
-    [nr,nc] = size(f4);
-    cplx_mode = 'real';
+    f           = 1:2^20;
+    ssin        = sin(f);
+    snoi        = normrnd(0,0.1,2^20,1);
+    input_data  = ssin' + snoi;
+    [nr,nc]     = size(input_data);
+    cplx_mode   = 'real';
 else
-    f_pre = load(data_file);
+    f_pre       = load(data_file);
     [nr,nc] = size(f_pre);
     if nc == 1
-        f4 = f_pre;
-        cplx_mode = 'real';
+        input_data  = f_pre;
+        cplx_mode   = 'real';
     else
-        f4 = complex(f_pre(:,1),f_pre(:,2));
-        cplx_mode = 'complex';
+        input_data  = complex(f_pre(:,1),f_pre(:,2));
+        cplx_mode   = 'complex';
     end
 end
 
-fft_samples=2^smpl;
-fft_samples_half=fft_samples/2;
-number_dia=floor(length(f4)/fft_samples);
+input_data_lth      = length(input_data);
+fft_samples         = 2^smpl;
+fft_samples_half    = fft_samples/2;
+number_dia          = floor(input_data_lth/fft_samples);
+Fs2                 = Fs/2;
 
-fprintf('Total %d %s samples (FFT %d samples and %d full diapasons)\n',length(f4),cplx_mode,fft_samples,number_dia);
-fprintf('FFT precision: %d Hz (smpl = %d); MAX %d Hz (smpl = %d)\n',Fs*1e6/fft_samples,smpl,Fs*1e6/length(f4),log2(length(f4)));
+fprintf('Total %d %s samples (FFT %d samples and %d full diapasons)\n',input_data_lth,cplx_mode,fft_samples,number_dia);
+fprintf('FFT precision: %d Hz (smpl = %d); MAX %d Hz (smpl = %d)\n',Fs*1e6/fft_samples,smpl,Fs*1e6/input_data_lth,log2(input_data_lth));
 
 %Use Hann window or not
 if use_window == 1
@@ -95,28 +97,28 @@ end
 
 %Compute separate FFT for each part
 for k=1:number_dia
-    f0_s(k,:) = abs(fft(hann_coeffs.*f4(1+fft_samples*(k-1):fft_samples*k)));
+    fft_data_parts(k,:) = abs(fft(hann_coeffs.*input_data(1+fft_samples*(k-1):fft_samples*k)));
 end
 
 %Sum samples from each part and make logarithm
-f0_sum = sum(f0_s,1);
-f0_sum = 20*log10(f0_sum/max(f0_sum));
+fft_data_sum        = sum(fft_data_parts,1);
+fft_data_sum_log    = 20*log10(fft_data_sum/max(fft_data_sum));
 
 %Plot graphs
 if Fs > FS_FAST
-    fr=(1:fft_samples_half)/fft_samples_half*Fs/2+Fs/2;
-    for_plot = f0_sum(fft_samples_half+1:end);
+    spectre_freq_points = (1:fft_samples_half)/fft_samples_half*Fs2+Fs2;
+    spectre_data        = fft_data_sum_log(fft_samples_half+1:end);
 else
     if nc == 1
-        fr=(1:fft_samples_half)/fft_samples_half*Fs/2;
-        for_plot = f0_sum(1:fft_samples_half);
+        spectre_freq_points = (1:fft_samples_half)/fft_samples_half*Fs2;
+        spectre_data        = fft_data_sum_log(1:fft_samples_half);
     else
-        fr=(1:fft_samples)/fft_samples*Fs-Fs/2;
-        for_plot = [f0_sum(fft_samples_half+1:end) f0_sum(1:fft_samples_half)];
+        spectre_freq_points = (1:fft_samples)/fft_samples*Fs-Fs2;
+        spectre_data        = [fft_data_sum_log(fft_samples_half+1:end) fft_data_sum_log(1:fft_samples_half)];
     end
 end
 
-plot(fr,for_plot,plot_color)
+plot(spectre_freq_points,spectre_data,plot_color)
 axis('tight')
 grid on
 hold off
